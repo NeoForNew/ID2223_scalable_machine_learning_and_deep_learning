49a97807f7ddea667c2db8824d78179e5b8904c9
Zhenghong Xiao <93341119+NeoForNew@users.noreply.github.com>
2022-12-08 21:26:12 +0100

    Update app.py

--- Lab2/app.py
+++ Lab2/app.py
@@ -1,18 +1,36 @@
-from transformers import pipeline
 import gradio as gr
+from transformers import pipeline
+from pytube import YouTube
 
-pipe = pipeline(model="NeoonN/ID2223_Lab2_Whisper")  # change to "your-username/the-name-you-picked"
+pipe = pipeline(model="NeoonN/ID2223_Lab2_Whisper")
 
-def transcribe(audio):
-    text = pipe(audio)["text"]
-    return text
+def transcribe(audio, url):
+    """
+    Transcribes a YouTube video if a url is specified and returns the transcription.
+    If not url is specified, it transcribes the audio file as passed by Gradio.
+    :param audio: Audio file as passed by Gradio. Only used if no url is specified.
+    :param url: YouTube URL to transcribe.
+    :param seconds_max: Maximum number of seconds to consider. If the audio file is longer than this, it will be truncated.
+    """
+    if url:
+      video=YouTube(url).streams.filter(only_audio=True).all()
+      audio=video[0].download()
+      text = pipe(audio)["text"]
+      return text
+
+    else:
+        text = pipe(audio)["text"]
+        return text
 
 iface = gr.Interface(
     fn=transcribe, 
-    inputs=gr.Audio(source="microphone", type="filepath"), 
+    inputs=[
+        gr.Audio(source="microphone", type="filepath", label="Transcribe from Microphone"),
+        gr.Text(max_lines=1, placeholder="Enter YouTube Link with Chinese speech to be transcribed", label="Transcribe from YouTube URL"),
+    ], 
     outputs="text",
-    title="Whisper Small Hindi",
-    description="Realtime demo for Hindi speech recognition using a fine-tuned Whisper small model.",
+    title="Whisper Small Chinese",
+    description="Realtime demo for Chinese speech recognition using a fine-tuned Whisper small model.",
 )
 
 iface.launch()
