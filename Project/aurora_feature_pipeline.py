214fe7c8734eb82efa8669dc4fd1ce83c098aa74
Zhenghong <xzhe@kth.se>
2022-12-30 14:03:27 +0100

    update csv

--- Project/aurora_feature_pipeline.py
+++ Project/aurora_feature_pipeline.py
@@ -18,8 +18,8 @@ def g():
     project = hopsworks.login()
     fs = project.get_feature_store()
     df_features_no_onehot = pd.read_csv("https://raw.githubusercontent.com/NeoForNew/ID2223_scalable_machine_learning_and_deep_learning/main/Project/df_Features.csv") 
-    aurora_no_fg = fs.get_or_create_feature_group(
-    name="aurora_model",
+    aurora_no_fg = fs.create_feature_group(
+    name="aurora",
     version=1,
     description="aurora data",
     primary_key = ["Kp_index","cloudcover","visibility","clear_day","clear_night","cloudy","fog","partly_cloudy_day","partly_cloudy_night","rain1","snow1","wind","Clear","Overcast","Partially_cloudy","Rain","Rain_Overcast","Rain_Partially_cloudy","Snow","Snow_Overcast","Snow_Partially_cloudy","Snow_Rain","Snow_Rain_Overcast","Snow_Rain_Partially_cloudy"]
